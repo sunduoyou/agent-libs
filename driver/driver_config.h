0abf6acd79d9ff3bad05212c736fc220cd5b9f19
sanyangji <songyujie@zju.edu.cn>
2022-01-20 21:00:38 +0800

    add files to make directly

---
+++ driver/driver_config.h
@@ -0,0 +1,19 @@
+/*
+
+Copyright (C) 2021 The Falco Authors.
+
+This file is dual licensed under either the MIT or GPL 2. See MIT.txt
+or GPL2.txt for full copies of the license.
+
+*/
+#pragma once
+
+#define PROBE_VERSION "0.1.1dev"
+
+#define PROBE_NAME "kindling-falcolib"
+
+#define PROBE_DEVICE_NAME "kindling-falcolib"
+
+#ifndef KBUILD_MODNAME
+#define KBUILD_MODNAME PROBE_NAME
+#endif
